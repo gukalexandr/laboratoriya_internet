583759dca8fd58bb6f369f047a9b54b79c6d1d0c
Guk Alexandr <9033208833@bk.ru>
2023-11-10 14:02:36 +0400

    Add api method for autorisation user

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,10 +4,37 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Validator;
 
 class UserController extends Controller
 {
+    public function loginUser(Request $request): Response
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+
+        if(Auth::attempt($request->all())){
+
+            $user = Auth::user();
+
+            $success =  $user->createToken('MyApp')->plainTextToken;
+
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
     /**
      * Display a listing of the resource.
      */
